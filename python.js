fd376f61e618208eb72512cdf2331a9489c250f8
goudbhaskar600-star <goudbhaskar600@gmail.com>
2026-06-12 17:17:25 +0530

    Add files via upload

---
+++ python.js
@@ -0,0 +1,28 @@
+ const aboutBtn = document.getElementById("but2");
+
+aboutBtn.addEventListener("click", () => {
+    window.location.href = "../../About/about.html";
+});
+
+const homebtn = document.getElementById("but1");
+homebtn.addEventListener("click", () => {
+    window.location.href = "../../Home/index.html";
+});
+
+const skillsBtn = document.getElementById("but3");
+
+skillsBtn.addEventListener("click", () => {
+    window.location.href = "../skills.html";
+});
+
+const contactBtn = document.getElementById("but5");
+
+contactBtn.addEventListener("click", () => {
+    window.location.href = "../../Contact/contact.html";
+});
+
+const projectsBtn = document.getElementById("but4");
+
+projectsBtn.addEventListener("click", () => {
+    window.location.href = "../../Projects/projects.html";
+});
